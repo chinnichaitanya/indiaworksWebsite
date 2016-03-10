101d60b4ecf5621f7b1cd3b85bd364dfec7d16b1
chinnichaitanya <chchaitanya95@gmail.com>
2016-03-10 20:11:12 +0530

    initail commit  - folder structure  - basic layout

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name indiaworks16App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the indiaworks16App
+ */
+angular.module('indiaworks16App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name indiaworks16App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the indiaworks16App
+ */
+angular.module('indiaworks16App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
