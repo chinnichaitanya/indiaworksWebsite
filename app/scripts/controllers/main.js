dd71f3e530a455240c3fdfb9c022ee71a3d3d9ef
minu-aswanth <minuapple@gmail.com>
2016-03-13 03:05:31 +0530

    added service directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the indiaworks16App
  */
 angular.module('indiaworks16App')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.changeServiceHoverImage = function () {
+    	document.getElementById("serviceName").src="images/plumbing-01-onhover-01.png";
+    };
+
+    $scope.changeServiceHoverImageBack = function () {
+    	document.getElementById("serviceName").src="images/plumbing-01-01.png";
+    };
+
   });
---
+++ app/scripts/controllers/services.directive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('indiaworks16App')
+  .directive('serviceTemplate', function () {
+  	return{
+  		templateUrl: '../views/services-template.html',
+  		// link: function(scope, element, attr) {
+  		// 	element.css({
+  		// 		//backgroundImage:'url("images/plumbing-01-01.png")'
+  		// 	});
+	   //  	element.on('mouseover', function(event) {
+    // 	   		console.log("Came here");
+    // 	   		element.css({
+    // 	   			backgroundColor:'red'
+    // 	   		});
+    //  		});
+    // 	}
+	}
+  });
