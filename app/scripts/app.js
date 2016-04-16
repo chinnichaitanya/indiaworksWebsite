7129ebb291ecce31c57b19269c086ef49c6b6497
chinnichaitanya <chchaitanya95@gmail.com>
2016-04-17 01:55:31 +0530

    add auth-factory module for authorizations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,31 @@ angular
     localStorageServiceProvider
       .setPrefix('IndiaWorks');
   })
+  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
+    return {
+      // Add authorization token to headers
+      request: function (config) {
+        config.headers = config.headers || {};
+        if ($cookieStore.get('token')) {
+          config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+        }
+        return config;
+      },
+
+      // Intercept 401s and redirect you to login
+      responseError: function(response) {
+        if(response.status === 401) {
+          $location.path('/login');
+          // remove any stale tokens
+          $cookieStore.remove('token');
+          return $q.reject(response);
+        }
+        else {
+          return $q.reject(response);
+        }
+      }
+    };
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
